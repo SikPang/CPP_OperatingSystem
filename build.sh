5cb6b32ed38b1cde3f390e4fa7bda13f2e0451de
SikPang <songks91@naver.com>
2026-05-01 20:58:06 +0900

    kernel_08

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -xue
+
+# clang 경로와 컴파일 옵션
+CC=/usr/bin/clang
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fuse-ld=lld -fno-stack-protector -ffreestanding -nostdlib -mno-relax"
+
+# 커널 빌드
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
+    kernel.c common.c
--- run.sh
+++ run.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 set -xue
 
+sh build.sh
+
 # QEMU 실행 파일 경로
 QEMU=qemu-system-riscv32
-
-# clang 경로와 컴파일 옵션
-CC=/usr/bin/clang
-CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fuse-ld=lld -fno-stack-protector -ffreestanding -nostdlib"
-
-# 커널 빌드
-$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
-    kernel.c
 
 # QEMU 실행
 $QEMU -machine virt -bios default -nographic -serial stdio \
